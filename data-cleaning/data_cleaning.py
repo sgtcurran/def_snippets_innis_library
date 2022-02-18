103711e86458f87c4a5b18bf2f324550b6d58630
christopher <christopher.curran24@gmail.com>
2022-02-18 12:22:33 -0600

    version 1

---
+++ data-cleaning/data_cleaning.py
@@ -0,0 +1,9 @@
+#data cleaning 
+
+def clean_currency(x):
+    """ If the value is a string, then remove currency symbol and delimiters
+    otherwise, the value is numeric and can be converted
+    """
+    if isinstance(x, str):
+        return(x.replace('$', '').replace(',', ''))
+    return(x)
