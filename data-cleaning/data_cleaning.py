c310187d0c8696646bea7421eec32326e67a48b6
christopher <christopher.curran24@gmail.com>
2022-02-18 15:17:37 -0600

    add remove nan in panda DataFrame

--- data-cleaning/data_cleaning.py
+++ data-cleaning/data_cleaning.py
@@ -1,5 +1,5 @@
 #data cleaning 
-
+#remove currency 
 def clean_currency(x):
     """ If the value is a string, then remove currency symbol and delimiters
     otherwise, the value is numeric and can be converted
@@ -7,3 +7,16 @@ def clean_currency(x):
     if isinstance(x, str):
         return(x.replace('$', '').replace(',', ''))
     return(x)
+
+# Methods to replace NaN values with zeros in Pandas DataFrame:
+
+# For the whole DataFrame using pandas:
+pd.fillna(0) 
+# The dataframe.replace() function in Pandas can be defined as a simple method 
+# used to replace a string, regex, list, dictionary etc. in a DataFrame.
+df.replace(np.nan, 0) 
+
+#For one column using pandas:
+df['DataFrame Column'] = df['DataFrame Column'].fillna(0)
+# For one column using numpy:
+df['DataFrame Column'] = df['DataFrame Column'].replace(np.nan, 0)
